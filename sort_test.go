043925549f964cd42fc34649c154fa0807fb7105
Ryan Susana <rsusana@bol.com>
2022-07-17 17:18:51 +0200

    added more tests

--- sort_test.go
+++ sort_test.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
 func TestSortRows(t *testing.T) {
 
 	createNewLineRow := func(newLine int) *Row {
-		return rowWithStat("file10", map[string]interface{}{"newLine": 10})
+		return &Row{
+			Name: "file" + strconv.Itoa(newLine),
+			Data: map[string]interface{}{"newLine": newLine},
+		}
 	}
 	rowsUnsorted := []*Row{
 		createNewLineRow(10),
@@ -20,23 +24,17 @@ func TestSortRows(t *testing.T) {
 		createNewLineRow(60),
 	}
 	rowsSorted := []*Row{
-		createNewLineRow(10),
-		createNewLineRow(20),
-		createNewLineRow(30),
-		createNewLineRow(40),
-		createNewLineRow(50),
-		createNewLineRow(60),
 		createNewLineRow(70),
+		createNewLineRow(60),
+		createNewLineRow(50),
+		createNewLineRow(40),
+		createNewLineRow(30),
+		createNewLineRow(20),
+		createNewLineRow(10),
 	}
 
+	sortRows("newLine", rowsUnsorted)
 	for i, row := range rowsUnsorted {
 		assert.Equal(t, row.Data["newLine"], rowsSorted[i].Data["newLine"])
-	}
-}
-
-func rowWithStat(name string, data map[string]interface{}) *Row {
-	return &Row{
-		Name: name,
-		Data: data,
 	}
 }
