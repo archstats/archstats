38a07d24d019d5595e511bb78f6eb2e46e12c0f1
RyanSusana <ryansusana97@gmail.com>
2025-03-10 16:03:57 +0100

    Started with Kotlin (WIP)

--- extensions/treesitter/kotlin/kotlin.go
+++ extensions/treesitter/kotlin/kotlin.go
@@ -1,31 +1,30 @@
 package kotlin
 
-//
-//import (
-//	"github.com/archstats/archstats/extensions/treesitter/common"
-//	kotlin "github.com/fwcd/tree-sitter-kotlin/bindings/go"
-//	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-//)
-//
-//func createKotlinPack() *common.LanguagePack {
-//	language := tree_sitter.NewLanguage(kotlin.Language())
-//	template := &common.LanguagePackTemplate{
-//		FileGlob: "**.kt",
-//		Language: language,
-//		Queries: []string{
-//			`(import_header
-//	(identifier (((simple_identifier) ("\." (simple_identifier))*))@import )  @_import_no_wildcard .
-//)
-//
-//
-// `,
-//		},
-//	}
-//
-//	pack, err := common.PackFromTemplate(template)
-//	if err != nil {
-//		// print error panic
-//		panic(err)
-//	}
-//	return pack
-//}
+import (
+	"github.com/archstats/archstats/extensions/treesitter/common"
+	kotlin "github.com/fwcd/tree-sitter-kotlin/bindings/go"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+)
+
+func createKotlinPack() *common.LanguagePack {
+	language := tree_sitter.NewLanguage(kotlin.Language())
+	template := &common.LanguagePackTemplate{
+		FileGlob: "**.kt",
+		Language: language,
+		Queries: []string{
+			`(import_header
+	(identifier (((simple_identifier) ("\." (simple_identifier))*))@import )  @_import_no_wildcard .
+)
+
+
+`,
+		},
+	}
+
+	pack, err := common.PackFromTemplate(template)
+	if err != nil {
+		// print error panic
+		panic(err)
+	}
+	return pack
+}
--- extensions/treesitter/kotlin/kotlin_test.go
+++ extensions/treesitter/kotlin/kotlin_test.go
@@ -1,27 +1,31 @@
 package kotlin
 
-//import (
-//	_ "embed"
-//	"fmt"
-//	"testing"
-//)
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+// TODO FINISH
 //
-////go:embed TestFile.kt
-//var rawFile string
-//
-//func TestKotlin(t *testing.T) {
-//	pack := createKotlinPack()
-//
-//	content := pack.AnalyzeFileContent("TestFile.kt", []byte(rawFile))
-//	//expectedImports := []string{
-//	//	"io.javalin.http.servlet",
-//	//	"io.javalin.http.staticfiles",
-//	//	"io.javalin.repo",
-//	//	"java.net",
-//	//	"foo.bar",
-//	//	"foo.baz",
-//	//}
-//
-//	fmt.Println(content.Snippets)
-//
-//}
+//go:embed TestFile.kt
+var rawFile string
+
+func TestKotlin(t *testing.T) {
+	pack := createKotlinPack()
+
+	content := pack.AnalyzeFileContent("TestFile.kt", []byte(rawFile))
+	expectedImports := []string{
+		"io.javalin.http.servlet",
+		"io.javalin.http.staticfiles",
+		"io.javalin.repo",
+		"java.net",
+		"foo.bar",
+		"foo.baz",
+	}
+
+	fmt.Println(content)
+
+	fmt.Println(expectedImports)
+
+}
